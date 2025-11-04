0bb7894a30639676d6c8a02a315dc52d2fc041dd
Akanksha R&D Software Engg <ggids934@hotmail.com>
2025-11-04 17:44:33 +0530

    Fix Flask entry point for Render

--- app.py
+++ app.py
@@ -92,6 +92,8 @@ def progress(filename):
 # ----------------------------
 # Run Flask
 # ----------------------------
-if __name__ == "__main__":
-    app.run(debug=True)
+
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
